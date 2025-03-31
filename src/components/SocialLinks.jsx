128dbe0c610175e8f6bd0d6acfb17c2adc45d8a8
badri-2005 <badrinarayananramkumar@gmail.com>
2025-03-31 11:35:08 +0530

    fix download attribute for resume link in SocialLinks component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -42,19 +42,18 @@ const SocialLinks = () => {
             ),
             href: '/Badri-Resume.pdf',
             style: 'rounded-br-md',
-            download: true
         },
     ];
 
     return (
         <div className='hidden lg:flex flex-col top-[35%] left-0 fixed '>
             <ul>
-                {links.map(({ id, child, href, style, download }) => (
+                {links.map(({ id, child, href, style }) => (
                     <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + " " + style}>
                         <a
                             href={href}
                             className='flex items-center justify-between w-full text-white'
-                            download={download}
+                            download="Badri-Resume.pdf"
                             target='_blank'
                             rel='noreferrer'
                         >
