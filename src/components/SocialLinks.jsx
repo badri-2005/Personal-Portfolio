868cebd206fa9da9872c9ea8b1002439d6ce4a51
badri-2005 <badrinarayananramkuma@gmail.com>
2025-12-23 23:56:07 +0530

    add updated resume file and correct link in SocialLinks component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: '/BadriNarayanan-Resume.pdf',
+            href: '/Resume.pdf',
             style: 'rounded-br-md',
         },
     ];
